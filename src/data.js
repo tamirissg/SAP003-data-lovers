65f3a2f120b1f6e0e831aa417c2a61787a13b987
rosangelabsilva <rosangela-bsilva@hotmail.com>
2019-09-12 17:45:56 -0300

    adicionado função que mostra os dados na tela

--- src/data.js
+++ src/data.js
@@ -1,8 +1,29 @@
 // esta é uma função de exemplo
 // veja como agregamos a função ao objeto global window
 
-const example = () => {
-  return "example";
+const infoCharacters = (parentElement) => {
+  RICKANDMORTY.results.map(value => {
+
+    let info = [value.name, value.status, value.gender];
+
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+
+    let imagePath = value.image;
+    const img = document.createElement("img");
+    img.setAttribute("class", "card-image");
+    img.src = imagePath;
+    card.append(img);
+
+    info.map(item => {
+      const list = document.createElement("li");
+      list.setAttribute("class", "card-info");
+      list.innerHTML += item;
+      card.append(list);
+    });
+
+    parentElement.append(card);
+  });
 };
 
-window.example = example;
+window.infoCharacters = infoCharacters;
