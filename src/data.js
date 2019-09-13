032a4f422a25f86867ad8fff3f1a1877906d671c
tamirissg <tamiris@icamiabas.com.br>
2019-09-13 13:18:35 -0300

    alterações gerais

--- src/data.js
+++ src/data.js
@@ -1,8 +1,26 @@
-// esta é uma função de exemplo
-// veja como agregamos a função ao objeto global window
+const infoCharacters = (parentElement) => {
+  RICKANDMORTY.results.map(value => {
 
-const example = () => {
-  return "example";
+    let info = [value.name, value.status, value.gender];
+
+    const card = document.createElement("div");
+    card.setAttribute("class", "card");
+
+    let imagePath = value.image;
+    const img = document.createElement("img");
+    img.setAttribute("class", "card-image");
+    img.src = imagePath;
+    card.append(img);
+
+    info.map(item => {
+      const list = document.createElement("li");
+      list.setAttribute("class", "card-character");
+      list.innerHTML += item;
+      card.append(list);
+    });
+
+    parentElement.append(card);
+  });
 };
 
-window.example = example;
+window.infoCharacters = infoCharacters;
---
+++ src/main.js
@@ -0,0 +1,3 @@
+let result = document.getElementById("cardinfo");
+
+window.infoCharacters(result);
