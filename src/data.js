59c3aed42d1f1ad95c03f961a58903eb1e7d7f47
tamirissg <tamiris@icamiabas.com.br>
2019-09-17 17:15:46 -0300

    Alteração do código

--- src/data.js
+++ src/data.js
@@ -1,26 +1,29 @@
-const infoCharacters = (parentElement) => {
-  RICKANDMORTY.results.map(value => {
 
-    let info = [value.name, value.status, value.gender];
-
-    const card = document.createElement("div");
-    card.setAttribute("class", "card");
-
-    let imagePath = value.image;
-    const img = document.createElement("img");
-    img.setAttribute("class", "card-image");
-    img.src = imagePath;
-    card.append(img);
-
-    info.map(item => {
-      const list = document.createElement("li");
-      list.setAttribute("class", "card-character");
-      list.innerHTML += item;
-      card.append(list);
-    });
-
-    parentElement.append(card);
-  });
+function filterData(data, condition){
+  return data.filter(item => item.gender.includes(condition));
+  
+}
+const app ={
+  filterData
 };
 
-window.infoCharacters = infoCharacters;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,3 +1,74 @@
-let result = document.getElementById("cardinfo");
+const data = RICKANDMORTY.results;
+const menuFiltro = document.getElementById("filtro-tipos");
 
-window.infoCharacters(result);
+
+
+menuFiltro.addEventListener("change", funcao());
+
+function funcao() {
+   selecionados(app.filterData(data, menuFiltro.value));
+};
+
+
+window.onload = () => {
+    carregaMenuTipos(data);
+}
+
+function carregaMenuTipos(data) {
+    const rickandmortygender = [];
+    data.map(gender => {
+        if(!rickandmortygender.includes(gender)) {
+            rickandmortygender.push(gender);
+        } else {
+            return false;
+        }
+    });
+
+    menuFiltro.innerHTML = "";
+    menuFiltro.innerHTML = `<option value ="none">Selecionefiltro</option>`;
+    menuFiltro.innerHTML += rickandmortygender.map(item => 
+    `<option value="${item.gender}">${item.gender}</option>`).join("");
+
+}
+
+function selecionados(arr) {
+    const mostrarCardsDiv = document.getElementById ("main");
+    mostrarCardsDiv.innerHTML ="";
+    arr.forEach(data => {
+        mostrarCardsDiv.innerHTML += `
+        <div>
+          <img src="${data.image}"/>
+          <h4><p class ="name">Name:${data.name}</p></h4>
+          <h5><p class ="gender">Gender:${data.gender}</p></h5>
+          <h6><p class ="status">Status:${data.status}</p></h6>
+        </div> `
+    });
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
